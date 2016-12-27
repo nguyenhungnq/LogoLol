ee1c2c900d298dff225b52ca86eb4abdd5d9d16b
nguyenhungnq <NguyenHung@antonio.local>
2016-12-27 11:28:31 +0700

    done

--- LogoLol/ViewController.swift
+++ LogoLol/ViewController.swift
@@ -16,15 +16,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for x in 0...5 {
-            let image = UIImage(named: "Logo\(x).png")
-            images.append(UIImageView(image: image))
-        }
         
-        print("Count: \(images.count)")
         
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        print("Scrollview width: \(scrollView.frame.size.width)")
+        
+        let scrollWidth = scrollView.frame.size.width
+        for x in 0...5 {
+            let image = UIImage(named: "Logo\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            
+        }
+        
+        scrollView.backgroundColor = UIColor.purple
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+    }
 
 }
 
